ef265420f8e49347d2f5f826cb2c54979a887c41
Samantha Wolf <wolfsp@muohio.edu>
2012-09-04 14:56:21 -0400

    this is dying...

--- src/HW01orsomethingApp.cpp
+++ src/HW01orsomethingApp.cpp
@@ -19,19 +19,22 @@ class HW01orsomethingApp : public AppBasic {
 
 	private:
     Surface* my_surface_;
-	int red;
-	int green;
-	int blue;
+	int count;
+	int xIndex;
+	int yIndex;
 	static const int kAppWidth=800;
 	static const int kAppHeight=600;
 	static const int kTextureSize=1024;
+
 	
 };
 
 	void HW01orsomethingApp::setup()
 	{
 		my_surface_ = new Surface(kAppWidth, kAppHeight, false);
-	
+		count=0;
+		xIndex=kAppWidth/4;
+		yIndex=kAppHeight/4;
 	}
 	
 
@@ -45,23 +48,45 @@ class HW01orsomethingApp : public AppBasic {
 	}
 
 	//This method puts a rectangle in the middle of the screen
-	void drawRectangle(uint8_t* pixelData, int X, int Y, int width, int height, int color, int kAppWidth, int kAppHeight) {
-	for(int x= X; x<width; x++) {
-		for(int y=Y; y<height; y++) {
+	void drawRectangle(uint8_t* pixelData, int x, int y, int width, int height, int color, int kAppWidth, int kAppHeight) {
+	for(x; x<width; x++) {
+		for(y; y<height; y++) {
 			pixelData[3*(y*kAppWidth+x)]=color;
 			}
 		}
 	}
 
 
+	void clearBackground(uint8_t* data, int kAppWidth, int kAppHeight){
+    for(int i = 0; i<kAppWidth*kAppHeight*3; i++){
+        data[i] = 0;
+    }
+}
+	void drawLine(uint8_t* pixelData, int X, int Y, int length, int color, int kAppWidth, int kAppHeight) {
+		drawRectangle(pixelData, X, Y, length, 3, color, kAppWidth, kAppHeight);
+	}
+
 	void HW01orsomethingApp::update()
 	{
 	uint8_t* pixelData = (*my_surface_).getData();
-	drawRectangle(pixelData, kAppWidth/4, kAppHeight/4, kAppWidth/2, kAppHeight/2, 50, kAppWidth, kAppHeight);			
+	
+	//clearBackground(pixelData, kAppWidth, kAppHeight);
+	//drawRectangle(pixelData, xIndex, yIndex, xIndex*3, yIndex*3, 50, kAppWidth, kAppHeight);
+	drawRectangle(pixelData, 0,0, kAppWidth, kAppHeight, 255, kAppWidth, kAppHeight);
+	//count++;
+	/*if(count>5) {
+		count=0;
+		xIndex=kAppWidth/4;
+		yIndex=kAppHeight/4;
 
 	}
+	else {
+		xIndex=xIndex/2;
+		yIndex/2;
+	}
 	
-
+	*/
+	}
 
 	
 
